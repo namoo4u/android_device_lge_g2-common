5bdb632fb3b2352cd680b0f0dd0ad3098750bcf4
Tyler Dunn <robotdude@att.net>
2015-02-05 20:07:55 -0500

    g2: Remove some bloat

--- g2.mk
+++ g2.mk
@@ -102,9 +102,7 @@ PRODUCT_PACKAGES += \
 
 # Live Wallpapers
 PRODUCT_PACKAGES += \
-    LiveWallpapers \
     LiveWallpapersPicker \
-    VisualizationWallpapers \
     librs_jni
 
 # NFC packages
